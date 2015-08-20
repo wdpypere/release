d760707b31569a5aca70080a7e667f3c314fa443
Wouter Depypere <wouter.depypere@ugent.be>
2015-08-20 20:01:10 +0200

    save

--- src/releasing/quattorpoddoc.py
+++ src/releasing/quattorpoddoc.py
@@ -19,68 +19,66 @@ MAILREGEX = re.compile(("([a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^
                         "\sdot\s))+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)"))
 PATHREGEX = re.compile(r'(\s+)((?:/[\w{}]+)+\.?\w*)(\s*)')
 EXAMPLEMAILS = ["example", "username", "system.admin"]
-LOGGER = fancylogger.getLogger()
+logger = fancylogger.getLogger()
 DOCDIR = "docs"
 
 REPOMAP = {
     "configuration-modules-core": {"sitesubdir": "components",
                                    "prefix": "ncm-",
-                                   "target": "target"},
-    "aii": {"sitesubdir": "AII",
-            "prefix": "aii-",
-            "target": "target"},
+                                   "target": "/NCM/Component/"},
+    #"aii": {"sitesubdir": "AII",
+    #        "prefix": "aii-",
+    #        "target": " "},
     "CAF": {"sitesubdir": "CAF",
             "prefix": "",
-            "target": "doc/pod/CAF"},
+            "target": "/CAF/"},
     "CCM": {"sitesubdir": "CCM",
             "prefix": "",
-            "target": "doc/pod/EDG/WP4/CCM"},
-    "build-scripts": {"sitesubdir": "maven-tools",
-                    "prefix": "",
-                    "target": ""},
+            "target": "EDG/WP4/CCM/"},
+#    "build-scripts": {"sitesubdir": "maven-tools",
+#                    "prefix": "",
+#                    "target": ""},
     }
 
 
-def mavencleancompile(modules_location):
+def mavencleancompile(repo_location, repository):
     """
     Executes mvn clean and mvn compile in the given modules_location.
     """
+    repoloc = os.path.join(repo_location, repository)
+    logger.info("Doing maven clean in %s." % repoloc)
+    output = run_asyncloop("mvn clean", startpath=repoloc)
+    logger.debug(output)
 
-    LOGGER.info("Doing maven clean in %s." % modules_location)
-    output = run_asyncloop("mvn clean", startpath=modules_location)
-    LOGGER.debug(output)
-
-    LOGGER.info("Doing maven compile in %s." % modules_location)
-    output = run_asyncloop("mvn compile", startpath=modules_location)
-    LOGGER.debug(output)
+    logger.info("Doing maven compile in %s." % repoloc)
+    output = run_asyncloop("mvn compile", startpath=repoloc)
+    logger.debug(output)
 
 
-def generatemds(pods, location, subdir, prefix):
+def generatemds(repo, sources, location):     # , subdir, prefix):
     """
     Takes a list of components with podfiles and generates a md file for it.
     """
-    LOGGER.info("Generating md files.")
+    logger.info("Generating md files.")
     counter = 0
     mdfiles = []
 
-    comppath = os.path.join(location, DOCDIR, subdir)
+    comppath = os.path.join(location, DOCDIR , REPOMAP[repo]['sitesubdir']) 
     if not os.path.exists(comppath):
         os.makedirs(comppath)
 
-    for component in sorted(pods):
-        for pod in pods[component]:
-            component = component.replace(prefix, '')
-            title = os.path.splitext(os.path.basename(pod))[0]
-            if component == "target":
-                component = subdir
-            if component != title:
-                title = component+'::'+title
-            mdfile = os.path.join(comppath, '%s.md' % title)
-            convertpodtomarkdown(pod, mdfile)
-            counter += 1
+    for source in sources:
+        if source.endswith(".pan"):
+            mdfile = os.path.split(source)[-2] + "schema.md"
+            #print "found pan file, making pan annotations."
+        else:
+            sourcename = source.split(REPOMAP[repo]['target'])[-1]
+            mdfile = os.path.splitext(sourcename)[0].replace("/", "::").lower() + ".md"
+            
+            convertpodtomarkdown(source, os.path.join(comppath , mdfile))
             mdfiles.append(mdfile)
 
-    LOGGER.info("Written %s md files." % counter)
+    logger.info("Written %s md files." % len(mdfiles))
     return mdfiles
 
 
@@ -88,10 +86,10 @@ def convertpodtomarkdown(podfile, outputfile):
     """
     Takes a podfile and converts it to a markdown with the help of pod2markdown.
     """
-    LOGGER.debug("Running pod2markdown on %s." % podfile)
+    logger.debug("Running pod2markdown on %s." % podfile)
     output = run_asyncloop("pod2markdown %s" % podfile)
-    LOGGER.debug("writing output to %s." % outputfile)
-    LOGGER.debug(output)
+    logger.debug("writing output to %s." % outputfile)
+    logger.debug(output)
     with open(outputfile, "w") as fih:
         fih.write(output[1])
 
@@ -100,7 +98,7 @@ def generatetoc(pods, outputloc, indexname, subdir, prefix, title):
     """
     Generates a TOC for the parsed components.
     """
-    LOGGER.info("Generating TOC as %s." % os.path.join(outputloc, indexname))
+    logger.info("Generating TOC as %s." % os.path.join(outputloc, indexname))
 
     with open(os.path.join(outputloc, indexname), "w") as fih:
         fih.write("site_name: %s\n\n" % title)
@@ -127,10 +125,10 @@ def writeifexists(outputloc, linkname, subdir, fih):
     Checks if the MD exists before adding it to the TOC.
     """
     if os.path.exists(os.path.join(outputloc, DOCDIR, linkname)):
-        LOGGER.debug("Adding %s to toc." % linkname)
+        logger.debug("Adding %s to toc." % linkname)
         fih.write("- ['%s', '%s']\n" % (linkname, subdir))
     else:
-        LOGGER.warn("Expected %s but it does not exist. Not adding to toc."
+        logger.warn("Expected %s but it does not exist. Not adding to toc."
                     % os.path.join(outputloc, DOCDIR, linkname))
 
 
@@ -138,14 +136,14 @@ def removemailadresses(mdfiles):
     """
     Removes the email addresses from the markdown files.
     """
-    LOGGER.info("Removing emailaddresses from md files.")
+    logger.info("Removing emailaddresses from md files.")
     counter = 0
     for mdfile in mdfiles:
         with open(mdfile, 'r') as fih:
             mdcontent = fih.read()
         replace = False
         for email in re.findall(MAILREGEX, mdcontent):
-            LOGGER.debug("Found %s." % email[0])
+            logger.debug("Found %s." % email[0])
             replace = True
             if email[0].startswith('//'):
                 replace = False
@@ -154,167 +152,179 @@ def removemailadresses(mdfiles):
                     replace = False
 
         if replace:
-            LOGGER.debug("Removed it from line.")
+            logger.debug("Removed it from line.")
             mdcontent = mdcontent.replace(email[0], '')
             with open(mdfile, 'w') as fih:
                 fih.write(mdcontent)
             counter += 1
-    LOGGER.info("Removed %s email addresses." % counter)
+    logger.info("Removed %s email addresses." % counter)
 
 
 def removewhitespace(mdfiles):
     """
     Removes extra whitespace (\n\n\n).
     """
-    LOGGER.info("Removing extra whitespace from md files.")
+    logger.info("Removing extra whitespace from md files.")
     counter = 0
     for mdfile in mdfiles:
         with open(mdfile, 'r') as fih:
             mdcontent = fih.read()
         if '\n\n\n' in mdcontent:
-            LOGGER.debug("Removing whitespace in %s." % mdfile)
+            logger.debug("Removing whitespace in %s." % mdfile)
             mdcontent = mdcontent.replace('\n\n\n', '\n')
             with open(mdfile, 'w') as fih:
                 fih.write(mdcontent)
             counter += 1
-    LOGGER.info("Removed extra whitespace from %s files." % counter)
+    logger.info("Removed extra whitespace from %s files." % counter)
 
 
 def decreasetitlesize(mdfiles):
     """
     Makes titles smaller, e.g. replace "# " with "### ".
     """
-    LOGGER.info("Downsizing titles in md files.")
+    logger.info("Downsizing titles in md files.")
     counter = 0
     for mdfile in mdfiles:
         with open(mdfile, 'r') as fih:
             mdcontent = fih.read()
         if '# ' in mdcontent:
-            LOGGER.debug("Making titles smaller in %s." % mdfile)
+            logger.debug("Making titles smaller in %s." % mdfile)
             mdcontent = mdcontent.replace('# ', '### ')
             with open(mdfile, 'w') as fih:
                 fih.write(mdcontent)
             counter += 1
-    LOGGER.info("Downsized titles in %s files." % counter)
+    logger.info("Downsized titles in %s files." % counter)
 
 
 def removeheaders(mdfiles):
     """
     Removes MAINTAINER and AUTHOR headers from md files.
     """
-    LOGGER.info("Removing AUTHOR and MAINTAINER headers from md files.")
+    logger.info("Removing AUTHOR and MAINTAINER headers from md files.")
     counter = 0
     for mdfile in mdfiles:
         with open(mdfile, 'r') as fih:
             mdcontent = fih.read()
         if '# MAINTAINER' in mdcontent:
-            LOGGER.debug("Removing # MAINTAINER in %s." % mdfile)
+            logger.debug("Removing # MAINTAINER in %s." % mdfile)
             mdcontent = mdcontent.replace('# MAINTAINER', '')
             with open(mdfile, 'w') as fih:
                 fih.write(mdcontent)
             counter += 1
         if '# AUTHOR' in mdcontent:
-            LOGGER.debug("Removing # AUTHOR in %s." % mdfile)
+            logger.debug("Removing # AUTHOR in %s." % mdfile)
             mdcontent = mdcontent.replace('# AUTHOR', '')
             with open(mdfile, 'w') as fih:
                 fih.write(mdcontent)
             counter += 1
 
-    LOGGER.info("Removed %s unused headers." % counter)
+    logger.info("Removed %s unused headers." % counter)
 
 
 def codifypaths(mdfiles):
     """
     Puts paths inside code tags
     """
-    LOGGER.info("Putting paths inside code tags.")
+    logger.info("Putting paths inside code tags.")
     counter = 0
     for mdfile in mdfiles:
         with open(mdfile, 'r') as fih:
             mdcontent = fih.read()
 
-        LOGGER.debug("Tagging paths in %s." % mdfile)
+        logger.debug("Tagging paths in %s." % mdfile)
         mdcontent, counter = PATHREGEX.subn(r'\1`\2`\3', mdcontent)
         with open(mdfile, 'w') as fih:
             fih.write(mdcontent)
 
-    LOGGER.info("Code tagged %s paths." % counter)
+    logger.info("Code tagged %s paths." % counter)
 
 
-def checkinputandcommands(modloc, outputloc, runmaven):
+def checkinputandcommands(sourceloc, outputloc, runmaven):
     """
     Check if the directories are in place.
     Check if the required binaries are in place.
     """
-    LOGGER.info("Checking if the given paths exist.")
-    if not modloc:
-        LOGGER.error("Repo location not specified.")
+    logger.info("Checking if the given paths exist.")
+    if not sourceloc:
+        logger.error("Repo location not specified.")
         sys.exit(1)
     if not outputloc:
-        LOGGER.error("output location not specified")
+        logger.error("output location not specified")
         sys.exit(1)
-    if not os.path.exists(modloc):
-        LOGGER.error("Repo location %s does not exist" % modloc)
+    if not os.path.exists(sourceloc):
+        logger.error("Repo location %s does not exist" % sourceloc)
         sys.exit(1)
+    for repo in REPOMAP.keys():
+        if not os.path.exists(os.path.join(sourceloc, repo)):
+            logger.error("Repo location %s does not exist" % os.path.join(sourceloc, repo))
+            sys.exit(1)
     if not os.path.exists(outputloc):
-        LOGGER.error("Output location %s does not exist" % outputloc)
+        logger.error("Output location %s does not exist" % outputloc)
         sys.exit(1)
 
-    LOGGER.info("Checking if repo is supported.")
-    if not os.path.basename(os.path.normpath(modloc)) in REPOMAP:
-        LOGGER.error("Repo at %s is not supported. Supported repos are:" % modloc)
-        for supmod in REPOMAP:
-            LOGGER.error(" - %s" % supmod)
-        sys.exit(1)
-
-    LOGGER.info("Checking if required binaries are installed.")
+    logger.info("Checking if required binaries are installed.")
     if runmaven:
         if not which("mvn"):
-            LOGGER.error("The command mvn is not available on this system, please install maven.")
+            logger.error("The command mvn is not available on this system, please install maven.")
             sys.exit(1)
     if not which("pod2markdown"):
-        LOGGER.error("The command pod2markdown is not available on this system, please install pod2markdown.")
+        logger.error("The command pod2markdown is not available on this system, please install pod2markdown.")
         sys.exit(1)
 
 
-def listcomponents(module_location, prefix):
+def listperlmodules(module_location):
     """
-    return a list of components in module_location.
+    return a list of perl modules in module_location.
     """
-    components = []
-    counter = 0
-    LOGGER.info("Searching for components in %s." % module_location)
-    for posloc in os.listdir(module_location):
-        if os.path.isdir(os.path.join(module_location, posloc)) and posloc.startswith(prefix):
-            components.append(posloc)
-            LOGGER.debug("Adding %s to component list." % posloc)
-            counter += 1
-    LOGGER.info("Found %s components." % counter)
-    return components
+    listtt = []
+    for path, dirs, files in os.walk(module_location):
+        if is_wanted_dir(path, files):
+            for f in files:
+                if is_wanted_file(path, f):
+                    # pod takes preference over 
+                    fname = os.path.join(path, f)
+                    duplicate = ""
+                    if "doc/pod" in fname:
+                        duplicate = fname.replace('doc/pod', 'lib/perl')
+                        if f.endswith('.pod'):
+                            duplicate = duplicate.replace(".pod", ".pm")
+                        if duplicate in listtt:
+                            listtt[listtt.index(duplicate)] = fname
+                            continue
+                            
+                    duplicate = ""
+                    if "lib/perl" in fname:
+                        duplicate = fname.replace('lib/perl', 'doc/pod' )
+                        if f.endswith('.pm'):
+                            duplicate = duplicate.replace(".pm", ".pod" )
+                        if not duplicate in listtt:
+                            listtt.append(fname)
+                            continue
+                    listtt.append(os.path.join(path,f))
+                    
+    return listtt
 
+def is_wanted_dir(path, files):
+    #must be in target
+    if not 'target' in path: return False
+    #directory must have files
+    if len(files) == 0 : return False
+    #must qualify one of these
+    if not True in [ x in path for x in ["doc/pod", "lib/perl", "pan"]]: return False
+    return True
+    
+    
 
-def listpods(module_location, components, target):
-    """
-    return an array of the pod files per component.
-    """
-    comppods = {}
-    counter = 0
-    LOGGER.info("searching for podfiles per component.")
-    for comp in components:
-        pods = []
-        for root, _, files in os.walk(os.path.join(module_location, comp, target)):
-            for fileh in files:
-
-                if fileh.endswith(".pod"):
-                    LOGGER.debug("%s - %s - %s" % (comp, fileh, root))
-                    counter += 1
-                    pods.append(os.path.join(root, fileh))
-
-        comppods[comp] = pods
-    LOGGER.info("Found %s pod files." % counter)
-    LOGGER.debug(comppods)
-    return comppods
-
+def is_wanted_file(path, filename):
+    # extension says it is perl
+    if True in [ filename.endswith(ext) for ext in [".pod", ".pm", ".pl"]]: return True
+    # it is a schema 
+    if filename == "schema.pan": return True
+    # it is a file without extension, check if it is perl
+    if len(filename.split(".")) < 2: 
+        with open(os.path.join(path, filename), 'r') as f:
+            if 'perl' in f.readline(): return True
+    return False
 
 def which(command):
     """
@@ -327,7 +337,20 @@ def which(command):
 
     return found
 
+def main(repoloc, outputloc):
+    mdfiles = {}
+    for repo in REPOMAP.keys():
+        logger.info("Processing %s." % repo)
+        if GO.options.maven_compile:
+            logger.info("Doing maven clean and compile.")
+            mavencleancompile(repoloc, repo)
+        else:
+            logger.info("Skipping maven clean and compile.")
 
+        pmodules = listperlmodules(os.path.join(repoloc, repo))
+        mdfiles[REPOMAP[repo]['sitesubdir']] = generatemds(repo, pmodules, outputloc)
+        
+    print mdfiles
 if __name__ == '__main__':
     OPTIONS = {
         'modules_location': ('The location of the repo checkout.', None, 'store', None, 'm'),
@@ -341,40 +364,31 @@ if __name__ == '__main__':
         'codify_paths': ('Put paths inside code tags.', None, 'store_true', True, 'p'),
     }
     GO = simple_option(OPTIONS)
-    LOGGER.info("Starting main.")
+    logger.info("Starting main.")
 
     checkinputandcommands(GO.options.modules_location, GO.options.output_location, GO.options.maven_compile)
 
-    if GO.options.maven_compile:
-        LOGGER.info("Doing maven clean and compile.")
-        mavencleancompile(GO.options.modules_location)
-    else:
-        LOGGER.info("Skipping maven clean and compile.")
+    main(GO.options.modules_location, GO.options.output_location)
+        
 
-    SUBDIR = REPOMAP[os.path.basename(os.path.normpath(GO.options.modules_location))]['sitesubdir']
-    PREFIX = REPOMAP[os.path.basename(os.path.normpath(GO.options.modules_location))]['prefix']
-    TARGET = REPOMAP[os.path.basename(os.path.normpath(GO.options.modules_location))]['target']
-    NAME = os.path.basename(os.path.normpath(GO.options.modules_location))
+#    PODS = listpods(GO.options.modules_location, COMPS, TARGET)
 
-    COMPS = listcomponents(GO.options.modules_location, PREFIX)
-    PODS = listpods(GO.options.modules_location, COMPS, TARGET)
+#    MDS = generatemds(PODS, GO.options.output_location, SUBDIR, PREFIX)
+#    generatetoc(PODS, GO.options.output_location, GO.options.index_name, SUBDIR, PREFIX, NAME)
 
-    MDS = generatemds(PODS, GO.options.output_location, SUBDIR, PREFIX)
-    generatetoc(PODS, GO.options.output_location, GO.options.index_name, SUBDIR, PREFIX, NAME)
+#    if GO.options.remove_emails:
+#        removemailadresses(MDS)
 
-    if GO.options.remove_emails:
-        removemailadresses(MDS)
+#    if GO.options.remove_headers:
+#        removeheaders(MDS)
 
-    if GO.options.remove_headers:
-        removeheaders(MDS)
+#    if GO.options.small_titles:
+#        decreasetitlesize(MDS)
 
-    if GO.options.small_titles:
-        decreasetitlesize(MDS)
+#    if GO.options.remove_whitespace:
+#        removewhitespace(MDS)
 
-    if GO.options.remove_whitespace:
-        removewhitespace(MDS)
+#    if GO.options.codify_paths:
+#        codifypaths(MDS)
 
-    if GO.options.codify_paths:
-        codifypaths(MDS)
-
-    LOGGER.info("Done.")
+    logger.info("Done.")
